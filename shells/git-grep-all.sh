ba20b36b953a9510a7a6847dde722a5868cb8c65
Andreas Smedjebacka <Smedjebacka@users.noreply.github.com>
2020-10-14 16:43:23 +0200

    Add grep modifiers, like -i. Also add comments.

--- shells/git-grep-all.sh
+++ shells/git-grep-all.sh
@@ -1,1 +1,7 @@
-for i in */.git; do ( echo \#\# $i; cd $i/..; git grep $1; ); done
+# first arg: string or regex pattern
+# second arg, optional: git grep modifiers, eg "-i" for case-insensitive
+for i in */.git;
+do (
+    echo \#\# $i; cd $i/..; git grep $2 $1;
+    );
+done
